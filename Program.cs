a9460f77cca360238df8f0b2fd360074bd258b89
Stefan Bettaglio <sbettaglio@gmail.com>
2020-02-11 20:55:53 -0500

    final draft adventure mode

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace RockPaperScissors
 {
   class Program
@@ -10,8 +9,8 @@ namespace RockPaperScissors
       while (play)
       {
         //As a user, I should see a welcome message.
-        Console.WriteLine("Welcome to Rock, Paper, Scissors, Lizard, Spock!");
-        //As a user, I should see the options I can pick (i.e., "rock", "paper", "scissor", "lizard", "spock")
+        Console.WriteLine("Welcome to Rock, Paper, Scissors!");
+        //As a user, I should see the options I can pick (i.e., "rock", "paper", "scissor")
         Console.WriteLine("How tough do you want it?");
         Console.WriteLine("easy, normal or impossible?");
         var difficulty = Console.ReadLine().ToLower();
@@ -23,16 +22,17 @@ namespace RockPaperScissors
         if (difficulty == "normal")
         {
           Console.WriteLine("Choose Wisely");
-          Console.WriteLine("rock, paper, scissors, lizard or spock?");
+          Console.WriteLine("rock, paper or scissors?");
           var tool = Console.ReadLine().ToLower();
-          if (tool != "rock" && tool != "paper" && tool != "scissors" && tool != "lizard" && tool != "spock")
+          if (tool != "rock" && tool != "paper" && tool != "scissors")
           {
-            Console.WriteLine("Invalid Selection. Please pick rock, paper, scissors, lizard or spock");
+            Console.WriteLine("Invalid Selection. Please pick rock, paper or scissors");
+            Console.ReadLine();
             tool = Console.ReadLine();
           }
           //The computer should randomly decide one of the options
           Random rnd = new Random();
-          string[] choices = { "rock", "paper", "scissors", "lizard", "spock" };
+          string[] choices = { "rock", "paper", "scissors" };
           int mChoices = rnd.Next(choices.Length);
           Console.WriteLine("Your opponent choose: {0}", choices[mChoices]);
           var RanChoice = choices[mChoices];
@@ -50,14 +50,6 @@ namespace RockPaperScissors
           {
             Console.WriteLine("You Win!");
           }
-          else if (tool == "rock" && RanChoice == "lizard")
-          {
-            Console.WriteLine("You win!");
-          }
-          else if (tool == "rock" && RanChoice == "spock")
-          {
-            Console.WriteLine("The computer wins!");
-          }
           else if (tool == "paper" && RanChoice == "rock")
           {
             Console.WriteLine("You win!");
@@ -65,14 +57,6 @@ namespace RockPaperScissors
           else if (tool == "paper" && RanChoice == "scissors")
           {
             Console.WriteLine("The computer wins!");
-          }
-          else if (tool == "paper" && RanChoice == "lizard")
-          {
-            Console.WriteLine("The computer wins!");
-          }
-          else if (tool == "paper" && RanChoice == "spock")
-          {
-            Console.WriteLine("You win!");
           }
           else if (tool == "scissors" && RanChoice == "rock")
           {
@@ -82,150 +66,58 @@ namespace RockPaperScissors
           {
             Console.WriteLine("You win!");
           }
-          else if (tool == "scissors" && RanChoice == "lizard")
-          {
-            Console.WriteLine("You win!");
-          }
-          else if (tool == "scissors" && RanChoice == "spock")
-          {
-            Console.WriteLine("The computer wins!");
-          }
-          else if (tool == "lizard" && RanChoice == "rock")
-          {
-            Console.WriteLine("The computer wins!");
-          }
-          else if (tool == "lizard" && RanChoice == "paper")
-          {
-            Console.WriteLine("You win!");
-          }
-          else if (tool == "lizard" && RanChoice == "scissors")
-          {
-            Console.WriteLine("The computer wins!");
-          }
-          else if (tool == "lizard" && RanChoice == "spock")
-          {
-            Console.WriteLine("You win!");
-          }
-          else if (tool == "spock" && RanChoice == "rock")
-          {
-            Console.WriteLine("You win!");
-          }
-          else if (tool == "spock" && RanChoice == "paper")
-          {
-            Console.WriteLine("The computer wins!");
-          }
-          else if (tool == "spock" && RanChoice == "scissors")
-          {
-            Console.WriteLine("You win!");
-          }
-          else if (tool == "spock" && RanChoice == "lizard")
-          {
-            Console.WriteLine("The computer wins!");
-          }
-
           //add some validation around user input.
         }
         else if (difficulty == "easy")
         {
           Console.WriteLine("Choose Wisely");
-          Console.WriteLine("rock, paper, scissors, lizard or spock?");
+          Console.WriteLine("rock, paper or scissors?");
           var tool = Console.ReadLine().ToLower();
-          if (tool != "rock" && tool != "paper" && tool != "scissors" && tool != "lizard" && tool != "spock")
+          if (tool != "rock" && tool != "paper" && tool != "scissors")
           {
             Console.WriteLine("Invalid Selection. Please pick rock, paper or scissors");
             tool = Console.ReadLine().ToLower();
           }
           if (tool == "rock")
           {
-            Random rnd = new Random();
-            string[] rockWins = { "scissors", "lizard" };
-            int iRockWins = rnd.Next(rockWins.Length);
-            Console.WriteLine("Your opponent chose {0}", rockWins[iRockWins]);
+            Console.WriteLine("Your opponent chose scissors.");
             Console.WriteLine("You Win!");
           }
           else if (tool == "paper")
           {
-            Random rnd = new Random();
-            string[] paperWins = { "spock", "rock" };
-            int iPaperWins = rnd.Next(paperWins.Length);
-            Console.WriteLine("Your opponent chose {0}", paperWins[iPaperWins]);
+            Console.WriteLine("Your opponent chose rock");
             Console.WriteLine("You win!");
           }
           else if (tool == "scissors")
           {
-            Random rnd = new Random();
-            string[] scissorsWin = { "lizard", "paper" };
-            int iScissorsWin = rnd.Next(scissorsWin.Length);
-            Console.WriteLine("Your opponent chose {0}", scissorsWin[iScissorsWin]);
+            Console.WriteLine("Your opponent chose paper");
             Console.WriteLine("You win!");
           }
-          else if (tool == "lizard")
-          {
-            Random rnd = new Random();
-            string[] lizardWins = { "spock", "paper" };
-            int iLizardWins = rnd.Next(lizardWins.Length);
-            Console.WriteLine("Your opponent chose {0}", lizardWins[iLizardWins]);
-            Console.WriteLine("You win!");
-          }
-          else if (tool == "spock")
-          {
-            Random rnd = new Random();
-            string[] spockWins = { "rock", "scissors" };
-            int iSpockWins = rnd.Next(spockWins.Length);
-            Console.WriteLine("Your opponent chose {0}", spockWins[iSpockWins]);
-            Console.WriteLine("You win!");
-          }
-
         }
         else if (difficulty == "impossible")
         {
           Console.WriteLine("Choose Wisely");
-          Console.WriteLine("rock, paper, scissors, lizard or spock?");
+          Console.WriteLine("rock, paper or scissors?");
           var tool = Console.ReadLine().ToLower();
-          if (tool != "rock" && tool != "paper" && tool != "scissors" && tool != "lizard" && tool != "spock")
+          if (tool != "rock" && tool != "paper" && tool != "scissors")
           {
             Console.WriteLine("Invalid Selection. Please pick rock, paper or scissors");
             tool = Console.ReadLine().ToLower();
           }
           if (tool == "rock")
           {
-            Random rnd = new Random();
-            string[] rockLoses = { "spock", "paper" };
-            int iRockLoses = rnd.Next(rockLoses.Length);
-            Console.WriteLine("Your opponent chose {0}", rockLoses[iRockLoses]);
+            Console.WriteLine("Your opponent chose paper.");
             Console.WriteLine("You lose");
           }
           else if (tool == "paper")
           {
-            Random rnd = new Random();
-            string[] paperLoses = { "lizard", "scissors" };
-            int iPaperLoses = rnd.Next(paperLoses.Length);
-            Console.WriteLine("Your opponent chose {0}", paperLoses[iPaperLoses]);
+            Console.WriteLine("Your opponent chose scissors");
             Console.WriteLine("You lose!");
           }
           else if (tool == "scissors")
           {
-            Random rnd = new Random();
-            string[] scissorsLose = { "spock", "rock" };
-            int iScissorsLose = rnd.Next(scissorsLose.Length);
-            Console.WriteLine("Your opponent chose {0}", scissorsLose[iScissorsLose]);
+            Console.WriteLine("Your opponent chose rock");
             Console.WriteLine("You lose");
-          }
-          else if (tool == "lizard")
-          {
-            Random rnd = new Random();
-            string[] lizardLoses = { "rock", "scissors" };
-            int iLizardLoses = rnd.Next(lizardLoses.Length);
-            Console.WriteLine("Your opponent chose {0}", lizardLoses[iLizardLoses]);
-            Console.WriteLine("You lose!");
-          }
-          else if (tool == "spock")
-          {
-            Random rnd = new Random();
-            string[] spockLoses = { "lizard", "paper" };
-            int iSpockLoses = rnd.Next(spockLoses.Length);
-            Console.WriteLine("Your opponent chose {0}", spockLoses[iSpockLoses]);
-            Console.WriteLine("You lose!");
           }
         }
         Console.WriteLine("Play again? ('yes' or 'no')");
@@ -241,7 +133,6 @@ namespace RockPaperScissors
           play = false;
         }
       }
-
     }
   }
 }
